1f80afd7fb07cbd37f0a8f9878a30753b6fadda2
lethosor <stahlalan19@gmail.com>
2013-08-20 16:56:51 -0400

    Make change password form modal

--- web-interface/resources/interface.js
+++ web-interface/resources/interface.js
@@ -3,6 +3,61 @@
  */
 
 $(function(){
+	function ModalWindow(node, opts) {
+		var self = {};
+		opts = $.extend({
+			'overlay': true,
+			'animation_dur': 500,
+			'hidden': true
+		}, opts);
+		self.animation_dur = opts.animation_dur;
+		// Make sure node is a single jQuery element
+		node = $(node);
+		if (node.length > 1) {
+			node = node.first();
+		}
+		node.addClass('modal-window');
+		self.node = node;
+		// Create an overlay
+		if (opts.overlay) {
+			self.overlay = $('<div>').addClass('overlay').appendTo(node.parent());
+		}
+		else {
+			self.overlay = $('<div>');
+		}
+		
+		if (opts.hidden) {
+			self.node.hide(0);
+			self.overlay.hide(0);
+		}
+		
+		self.hide = function(dur){
+			if (dur === undefined) {
+				dur = self.animation_dur;
+			}
+			self.node.fadeOut(dur);
+			self.overlay.fadeOut(dur);
+		};
+		self.show = function(dur){
+			if (dur === undefined) {
+				dur = self.animation_dur;
+			}
+			self.node.fadeIn(dur);
+			self.overlay.fadeIn(dur);
+		}
+		
+		self.close_button = $('<a>').addClass('close button').html('&times;')
+			.prependTo(self.node).css({'float':'right'}).click(function(e){
+				e.preventDefault(); self.hide();
+			}).attr('href', '#');
+		
+		if (this instanceof ModalWindow) $.extend(this, self); return self;
+	}
+	
+	/*
+	 * Add a close/hide button to error messages
+	 * Clicking the button causes the message to fade out
+	 */
 	$('.admin_flash').each(function(i, message){
 		var hide_button = $('<a>').attr({href:'#'}).html('&times;')
 			.appendTo($(message).find('p')).click(function(e){
@@ -10,4 +65,18 @@ $(function(){
 				$(message).fadeOut(500);
 			}).addClass('close button');
 	});
+	
+	/*
+	 * Change the change password form to be 'modal', hidden by default.
+	 */
+	change_password_form = ModalWindow('.admin_password_form');
+	if ($('.admin_flash').length) {
+		change_password_form.show(0);
+	}
+	change_password_button = $('<button>').text('Change password')
+		.insertBefore($('form[action=logout] input'))
+		.click(function(e){
+			e.preventDefault();
+			change_password_form.show();
+		})
 });
