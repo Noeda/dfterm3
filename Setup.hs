2effc45298bc16a2b4047abf37d5e1f90464524c
Mikko Juola <mikjuo@gmail.com>
2013-09-27 14:18:37 +0900

    Add icons.
    Icons generously provided by flabort from the bay12forums. We have a 32x32 and
    16x16 icons now. The icons are only used on Windows at the moment.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,24 @@
+{-# LANGUAGE CPP #-}
+
 import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+import Distribution.Simple.Program.Types
+import Distribution.Verbosity
+import Distribution.Simple.Utils
+
+main = defaultMainWithHooks myHooks
+
+myHooks :: UserHooks
+myHooks = simpleUserHooks
+    { preBuild = myPreBuild }
+
+myPreBuild :: Args -> BuildFlags -> IO HookedBuildInfo
+myPreBuild args bf = do
+#ifdef mingw32_HOST_OS
+    _ <- rawSystemExitCode normal "windres"
+                  ["dfterm3.rc", "-O", "coff", "icon/icon.o"]
+#endif
+    (preBuild simpleUserHooks) args bf
+
